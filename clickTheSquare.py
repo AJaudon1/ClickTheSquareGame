4af8f286712a8d7e93d62762440a70d0e2cf7aa8
AJaudon1 <150720037+AJaudon1@users.noreply.github.com>
2025-04-04 22:20:45 -0400

    Basic Game Functionality

--- clickTheSquare.py
+++ clickTheSquare.py
@@ -7,32 +7,22 @@ import time
 
 # Initialize pygame
 pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load("How_Far.mp3")
-pygame.mixer.music.play(-1)
 
 #Game settings
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Click the Square")
-font = pygame.font.SysFont(None, 36)
+#2 font sizes to choose from
+font1 = pygame.font.SysFont(None, 36)
+font2 = pygame.font.SysFont(None, 56)
 clock = pygame.time.Clock()
-square_size = 100
 
-
-#used for changing logo colors
-left_poly_color = "White"
-right_poly_color = "White"
-top_poly_color = "White"
-change_interval = 2650
-last_time = pygame.time.get_ticks()
-
-#class for easily making buttons
+#class for easily making menu buttons
 class Button:
-    def __init__(self, x, y, width, height, text, font, color):
+    def __init__(self, x, y, width, height, text, text_color, font, color):
         self.rect = pygame.Rect(x,y,width,height)
         self.text = text
         self.font = font
-        self.text_surface = self.font.render(text, True, "Black")
+        self.text_surface = self.font.render(text, True, text_color)
         self.color = color
 
     def draw(self, surface):
@@ -45,29 +35,94 @@ class Button:
 
     def is_on(self, mouse_pos):
         return self.rect.collidepoint(mouse_pos)
-    
 
-#Difficulty based on set grid_size
-grid_size = 3
+#class used for the squares in the game
+class Square:
+    def __init__(self, x, y, width, color):
+        self.rect = pygame.Rect(x,y,width, width)
+        self.color = color
+        self.is_target = False
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
+    def change_color(self, color):
+        self.color = color
+
+    def is_on(self, mouse_pos):
+        return self.rect.collidepoint(mouse_pos)
 
 #function to draw the main game grid
-#def draw_grid(grid_size):
-    #
+#grid will be approximately 640 x 640 pixels
+def draw_grid(grid_size):
+    grid = []
+    set_width = (640/grid_size) * 0.75
+    xy_adjust = (640/grid_size) * 0.25
+    
+    for row in range(grid_size):
+        grid_row = []
+        for col in range(grid_size):
+            #convert float values to integers for the position
+            x = int(350 + col*set_width + col*xy_adjust)
+            y = int(70 + row*set_width + row*xy_adjust)
 
-# game loop
+            #creates one square to add to the grid row
+            square = Square(x, y, int(set_width), "White")
+            grid_row.append(square)
+        #adds single row to the grid
+        grid.append(grid_row)
+    return grid
+
+def rand_index(grid_size):
+    num = random.randint(0, grid_size-1)
+    return num
+
+#used for testing
+def draw_square():
+    set_width = (640/grid_size) * 0.75
+    xy_adjust = (640/grid_size) * 0.25
+    x = int(320)
+    y = int(40)
+    square = Square(x, y, int(set_width), "White")
+    return square
+
+
+#Difficulty based on set grid_size
+grid_size = 5
+
+#used for changing logo colors
+left_poly_color = "White"
+right_poly_color = "White"
+top_poly_color = "White"
+change_interval = 2650
+last_time = pygame.time.get_ticks()
+
+# game loops
 game_running = True
+start_game = False
 
 #initialize menu buttons
-start_button = Button(480, 360, 320, 54, "Start", font, "White")
+start_button = Button(480, 360, 320, 54, "Start", "Black", font1, "White")
 
-settings_button = Button(480, 468, 320, 54, "Settings", font, "White")
+settings_button = Button(480, 468, 320, 54, "Settings", "Black", font1, "White")
 
-info_button = Button(480, 576, 320, 54, "Info", font, "White")
+info_button = Button(480, 576, 320, 54, "Info", "Black", font1, "White")
 
 button_hover = False
 
+
+#initialize game music
+pygame.mixer.init()
+pygame.mixer.music.load("How_Far.mp3")
+pygame.mixer.music.play(-1)
+
 while game_running:
     screen.fill("black")
+
+    #turn on main menu music if off
+    if not pygame.mixer.music.get_busy():
+        pygame.mixer.music.load("How_Far.mp3")
+        pygame.mixer.music.play(-1)
 
     #if mouse is not over buttons
     if button_hover == False:
@@ -90,7 +145,97 @@ while game_running:
         #check clicks
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if start_button.is_on(pygame.mouse.get_pos()):
-                game_running = False
+                pygame.mixer.music.stop()
+                pygame.mixer.music.load("Seeing_Squares.mp3")
+                pygame.mixer.music.play(-1)
+
+                #Gameplay starts here
+    #----------------------------------------------------------
+                game_grid = []
+                game_grid = draw_grid(grid_size)
+                points = 0
+
+                #Creates a timed countdown with text
+                countdown_int = 1000
+                countdown_last = pygame.time.get_ticks()
+                countdown_cur = pygame.time.get_ticks()
+                ready = Button(520, 340, 40, 40, "Ready...", "White", font2, "Black")
+                set = Button(645, 340, 40, 40, "Set...", "White", font2, "Black")
+                go = Button(740, 340, 40, 40, "Go!!!", "White", font2, "Black")
+                ready_on = True
+                set_on = False
+                go_on = False
+                num = 0
+
+                in_countdown = True
+                while in_countdown:
+                    screen.fill("Black")
+                    countdown_cur = pygame.time.get_ticks()
+                    if countdown_cur - countdown_last >= countdown_int:
+                        #increment counter and reset timer
+                        num += 1
+                        countdown_last = countdown_cur
+                    
+                    #check for the text to display
+                    if num == 1:
+                        set_on = True
+                    elif num == 2:
+                        go_on = True
+                    elif num == 3:
+                        in_countdown = False
+
+                    #draw countdown text if able
+                    if ready_on:
+                        ready.draw(screen)
+                    if set_on:
+                        set.draw(screen)
+                    if go_on:
+                        go.draw(screen)
+
+                    pygame.display.flip()
+                    clock.tick(30)
+
+                target_on = False
+                start_game = True
+                while start_game:
+                    screen.fill("Black")
+                    
+                    #turn on a target if there isn't one
+                    #DOUBLE PICKS CURRENTLY LOOK LIKE DROPPED INPUTS
+                    #PROVIDE FEEDBACK FOR CLICKS NEXT, GREEN FLASH?
+                    if target_on == False:
+                        row = rand_index(grid_size)
+                        col = rand_index(grid_size)
+                        game_grid[row][col].is_target = True
+                        game_grid[row][col].change_color("Red")
+                        target_on = True
+
+                    #draw grid each frame
+                    for row in range(grid_size):
+                        for col in range(grid_size):
+                            game_grid[row][col].draw(screen)
+                    
+                    #check for click events
+                    for event in pygame.event.get():
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            #iterate through the grid to find the clicked square
+                            for row in range(grid_size):
+                                for col in range(grid_size):
+                                    if game_grid[row][col].is_on(pygame.mouse.get_pos()):
+                                        if game_grid[row][col].is_target == True:
+                                            points += 1
+                                            game_grid[row][col].is_target = False
+                                            game_grid[row][col].change_color("White")
+                                            target_on = False
+                                        else:
+                                            start_game = False
+                                            print("You scored" + str(points) + "points")
+
+                    #grid updates at 30 fps
+                    pygame.display.flip()
+                    clock.tick(30)
+                pygame.mixer.music.stop()
+    #----------------------------------------------------------
             elif settings_button.is_on(pygame.mouse.get_pos()):
                 game_running = False
             elif info_button.is_on(pygame.mouse.get_pos()):
@@ -156,31 +301,9 @@ while game_running:
         #reset timer
         last_time = current_time
 
-
-    #draw menu buttons and text
-    #start_button = pygame.draw.rect(screen, "White", (480, 360, 320, 54))
-    #start_text = font.render("Start", True, "Black")
-    #start_rect = start_text.get_rect(center=start_button.center)
-    #screen.blit(start_text, start_rect)
-
-    #settings_button = pygame.draw.rect(screen, "White", (480, 468, 320, 54))
-    #settings_text = font.render("Settings", True, "Black")
-    #settings_rect = settings_text.get_rect(center=settings_button.center)
-    #screen.blit(settings_text, settings_rect)
-
-    #info_button = pygame.draw.rect(screen, "White", (480, 576, 320, 54))
-    #info_text = font.render("Info", True, "black")
-    #info_rect = info_text.get_rect(center=info_button.center)
-    #screen.blit(info_text, info_rect)
-
-    #check for menu clicks
-
-
     #30 fps
     pygame.display.flip()
     clock.tick(30)
 
 #close the game
 pygame.quit()
-
-
